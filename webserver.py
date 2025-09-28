516b14157a5af2736c4cba1cda69e317d5d78ff5
mirou133s <saci16mohamed16amir@gmail.com>
2025-09-28 22:36:56 +0100

    webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,18 @@
-from flask import flask 
-from threading import thread 
+frofrom flask import Flask
+from threading import Thread 
 
-app =flask('')
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Discord rahom mlih"
 
 def run():
-        app.run(host='0.0.0.0', port=10000, debug=False)
+    app.run(host='0.0.0.0', port=10000, debug=False)
 
+def keep_alive(): 
+    t = Thread(target=run)
+    t.start()
 
-    def keep_alive(): 
-        t = Thread(target=run)
-        t.start()
+# ابدأ التشغيل
+keep_alive()
